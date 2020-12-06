3ae41db2309dc5bb80d285eebc0fbb8b06de6411
Payton Swick <payton@foolord.com>
2020-12-06 13:30:48 -0500

    Make siteId a string or undefined in existingCardProcessor

--- client/my-sites/checkout/composite-checkout/composite-checkout.tsx
+++ client/my-sites/checkout/composite-checkout/composite-checkout.tsx
@@ -492,7 +492,7 @@ export default function CompositeCheckout( {
 						country: countryCode,
 						postalCode,
 						subdivisionCode,
-						siteId: siteId ? String( siteId ) : '',
+						siteId: siteId ? String( siteId ) : undefined,
 						domainDetails,
 					} ),
 					dataForProcessor
