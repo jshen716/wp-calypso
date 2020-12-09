6a319c91989a7cbd3ed5df23814e8210d44066be
Markus Dobmann <m.do@posteo.net>
2020-12-09 11:43:19 +0100

    add `removeViewer` functionality

--- client/state/viewers/actions.js
+++ client/state/viewers/actions.js
@@ -5,6 +5,9 @@ import {
 	VIEWERS_REQUEST,
 	VIEWERS_REQUEST_SUCCESS,
 	VIEWERS_REQUEST_FAILURE,
+	REMOVE_VIEWER,
+	REMOVE_VIEWER_SUCCESS,
+	REMOVE_VIEWER_FAILURE,
 } from 'calypso/state/action-types';
 
 import 'calypso/state/data-layer/wpcom/viewers';
@@ -27,5 +30,25 @@ export const requestViewersFailure = ( siteId, query, error ) => ( {
 	type: VIEWERS_REQUEST_FAILURE,
 	siteId,
 	query,
+	error,
+} );
+
+export const removeViewer = ( siteId, viewerId ) => ( {
+	type: REMOVE_VIEWER,
+	siteId,
+	viewerId,
+} );
+
+export const removeViewerSuccess = ( siteId, viewerId, data ) => ( {
+	type: REMOVE_VIEWER_SUCCESS,
+	siteId,
+	viewerId,
+	data,
+} );
+
+export const removeViewerFailure = ( siteId, viewerId, error ) => ( {
+	type: REMOVE_VIEWER_FAILURE,
+	siteId,
+	viewerId,
 	error,
 } );
